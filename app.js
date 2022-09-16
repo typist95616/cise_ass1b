9b403550c9ff052914236805acb24aa3659b9e8f
Jackie Lai <jackie12051995@gmail.com>
2022-09-16 21:27:49 +1200

    test

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 5051;
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 if(process.env.NODE_ENV === "production"){
-    app.get('/', (req, res) => {
-        req.sendFile(path.resolve(_dirname, 'build', 'index.html'));
-    })
+    // app.get('/', (req, res) => {
+    //     req.sendFile(path.resolve(_dirname, 'build', 'index.html'));
+    // })
 }
-app.get("/test", (req,res)=>{
-    res.send("Hello");
-})
+// app.get("/test", (req,res)=>{
+//     res.send("Hello");
+// })
 
 app.listen(port, (err) => {
     if(err) return console.log(err);
