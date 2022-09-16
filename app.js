edb89b28c17f062bdcef63e58b0421d669bacc12
Jackie Lai <jackie12051995@gmail.com>
2022-09-16 21:04:03 +1200

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('build'));
+    app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('*', (req, res) => {
         req.sendFile(path.resolve(_dirname, 'build', 'index.html'));
     })
