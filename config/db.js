50f1e5a3fbe895966e81cdbbd1cb8dcf8ad83c39
Jackie Lai <jackie12051995@gmail.com>
2022-09-17 20:10:59 +1200

    Connect DB

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://jackie:JackieLai@ciseass1b.dcb4ip8.mongodb.net/?retryWrites=true&w=majority", {
+    await mongoose.connect('mongodb+srv://jackie:JackieLai@ciseass1b.dcb4ip8.mongodb.net/?retryWrites=true&w=majority', {
       useNewUrlParser: true,
-      useFindAndModify: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
     });
 
     console.log("MongoDB Connection Success 👍");
   } catch (error) {
+    console.log(error);
     console.log("MongoDB Connection Failed 💥");
     process.exit(1);
   }
