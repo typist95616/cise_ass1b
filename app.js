3daa69fb71e5acd65c9a471df96f865653b0d7a3
Jackie Lai <jackie12051995@gmail.com>
2022-09-17 14:18:03 +1200

    test

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.use(express.json());
 const port = process.env.PORT || 5051;
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, '/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
     app.get("/APITesting", (req,res)=>{
         res.send("API running")
+    })
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }else{
     app.get("/test", (req,res)=>{
