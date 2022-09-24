935d2bd7d20901071c8836f206f137f3b30f6b54
Jackie Lai <jackie12051995@gmail.com>
2022-09-24 20:50:36 +1200

    Connect database by config.env

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const path = require("path");
+dotenv.config({path: path.join(__dirname , '../config.env')});
+
+const url = process.env.DatabaseURL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://jackie:JackieLai@ciseass1b.dcb4ip8.mongodb.net/?retryWrites=true&w=majority', {
+    console.log(process.env.DatabaseURL);
+    await mongoose.connect(url, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log("MongoDB Connection Success 👍");
   } catch (error) {
     console.log(error);
