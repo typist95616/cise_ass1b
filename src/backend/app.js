1cc7a0b87022db74eac1cd03a2f23661c9de0b22
BZ <zhangzishan119@gmail.com>
2022-10-19 12:56:20 +1300

    Updated app.js

--- src/backend/app.js
+++ src/backend/app.js
@@ -54,6 +54,14 @@ if(process.env.NODE_ENV === "production"){
         res.send("API running");
         console.log("test");
         res.end();
+    
+    app.use(express.static(path.join(__dirname, '../../build/'))); and
+
+    app.get('*', (req, res) => {
+        
+        res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+        
+    })
     })
 }else{
     app.get("/test", (req,res)=>{
